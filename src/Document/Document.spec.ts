cc0c20fba4e3bd8b490b36c9cfafcc37b26ab35c
Reece <reecemillsom@gmail.com>
2019-03-02 17:44:17 +0000

    initial commit for initial setup

---
+++ src/Document/Document.spec.ts
@@ -0,0 +1,12 @@
+import * as chai from 'chai';
+import { expect } from 'chai';
+
+describe("Document", () => {
+
+    it("will return true", () => {
+
+        expect(true).to.equal(true);
+
+    });
+
+});
