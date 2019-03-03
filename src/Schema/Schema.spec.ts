9cc97d2d2e234d52b9c8ff693b88bf414d3293b4
Reece <reecemillsom@gmail.com>
2019-03-03 12:22:25 +0000

    adding handling of getters and setters for virtuals

--- src/Schema/Schema.spec.ts
+++ src/Schema/Schema.spec.ts
@@ -1,93 +1,101 @@
-import * as chai from 'chai';
-import { expect } from 'chai';
-import { Schema } from 'mongoose';
-import SchemaHandler  from './Schema';
+import {expect} from 'chai';
+import {Schema} from 'mongoose';
+import {SchemaHandler, Type} from './Schema';
 
 describe("Schema", () => {
 
 	describe("when initialising", () => {
 
-	    describe("when a falsy definition is passed", () => {
+		describe("when a falsy definition is passed", () => {
 
-	        it("will return an error", () => {
+			it("will return an error", () => {
 
-	            expect(() =>
+				expect(() =>
 					new SchemaHandler(null)
 				).to.throw(Error, 'Invalid argument for definition parameter');
 
-	        });
+			});
 
-	    });
+		});
 
-	    describe("when schema definition is not of type object", () => {
+		describe("when schema definition is not of type object", () => {
 
-	        it("will throw an error", () => {
+			it("will throw an error", () => {
 
-	            expect(() =>
+				expect(() =>
 					new SchemaHandler([] as any, null)
 				).to.throw(Error, 'Invalid schema type, must be an object');
 
-	        });
+			});
 
-	    });
+		});
 
-	    describe("when definition object is empty and has no keys", () => {
+		describe("when definition object is empty and has no keys", () => {
 
-	        it("will throw an error", () => {
+			it("will throw an error", () => {
 
-	            expect(() =>
+				expect(() =>
 					new SchemaHandler({} as any)
 				).to.throw(Error, 'Invalid argument, definition object must contain contents');
 
-	        });
+			});
 
-	    });
+		});
 
-	    describe("when we have a valid definition", () => {
+		describe("when we have a valid definition", () => {
 
-	        it("will return a schema", () => {
+			it("will return a schema", () => {
 
-			  	const schemaHandler: SchemaHandler = new SchemaHandler({ name: String });
+				const schemaHandler: SchemaHandler = new SchemaHandler({name: String});
 
-		  		expect(schemaHandler.schema).to.be.instanceOf(Schema);
+				expect(schemaHandler.schema).to.be.instanceOf(Schema);
 
-	        });
+			});
 
-	    });
+		});
 
 	});
 
 	describe("when asked to attach virtuals", () => {
 
-	    it("will take the inputted function reference and virtual name and create a virtual on the schema", () => {
+		it("will take the inputted function reference and virtual name and create a virtual on the schema", () => {
 
-	        const schemaHandler: SchemaHandler = new SchemaHandler({ name: String, age: Number });
+			const schemaHandler: SchemaHandler = new SchemaHandler({name: String, age: Number});
 
-	        const someFunc = function () { console.log('some func') };
+			const someFunc = function () {
+				console.log('some func')
+			};
 
-	        schemaHandler.attachVirtuals([{ virtualFunction: someFunc, virtualName: 'someFunc' }]);
+			schemaHandler.attachVirtuals([{virtualFunction: someFunc, virtualName: 'someFunc', virtualType: Type.Set}]);
 
-	        const savedVirtual: any = schemaHandler.schema.virtual('someFunc');
+			const savedVirtual: any = schemaHandler.schema.virtual('someFunc');
 
-		  	expect(savedVirtual.path).to.equal('someFunc');
+			expect(savedVirtual.path).to.equal('someFunc');
+			expect(savedVirtual.setters[0]).to.be.a('function');
 
-	    });
+		});
 
-	    describe("when asked to attach multiple virtuals", () => {
+		describe("when asked to attach multiple virtuals", () => {
 
-	        it("will take the inputted functions and names and attach a virtual to the schema", () => {
+			it("will take the inputted functions and names and attach a virtual to the schema", () => {
 
-				const schemaHandler: SchemaHandler = new SchemaHandler({ name: String, age: Number });
+				const schemaHandler: SchemaHandler = new SchemaHandler({name: String, age: Number});
 
-				const someFunc1 = function () { console.log('some func 1') };
-				const someFunc2 = function () { console.log('some func 2') };
+				const someFunc1 = function () {
+					console.log('some func 1')
+				};
+				const someFunc2 = function () {
+					console.log('some func 2')
+				};
 
 				schemaHandler.attachVirtuals([{
-				  virtualFunction: someFunc1,
-				  virtualName: 'someFunc1'
+					virtualFunction: someFunc1,
+					virtualName: 'someFunc1',
+					virtualType: Type.Get
 				}, {
-				  virtualFunction: someFunc2,
-				  virtualName: 'someFunc2'
+					virtualFunction: someFunc2,
+					virtualName: 'someFunc2',
+					virtualType: Type.Get
 				}]);
 
 				const firstSavedVirtual: any = schemaHandler.schema.virtual('someFunc1');
@@ -96,9 +104,15 @@ describe("Schema", () => {
 				expect(firstSavedVirtual.path).to.equal('someFunc1');
 				expect(secondSavedVirtual.path).to.equal('someFunc2');
 
-	        });
+			});
 
-	    });
+		});
+
+	});
+
+	//TODO do this after handling getting and setting of virtuals not just get.
+	describe("when asked to attach instance methods", () => {
+
 
 	});
 
--- src/Schema/Schema.ts
+++ src/Schema/Schema.ts
@@ -1,55 +1,60 @@
-import { Schema, SchemaDefinition, SchemaOptions } from "mongoose";
-import { forEach } from 'lodash';
+import {Schema, SchemaDefinition, SchemaOptions} from "mongoose";
+import {forEach} from 'lodash';
+
+export enum Type {
+	Get = 'get',
+	Set = 'set'
+}
 
 export interface Virtual {
-  virtualFunction: any; //TODO this will be a function reference, see if there is a way around any type.
-  virtualName: string;
+	virtualFunction: any; //TODO this will be a function reference, see if there is a way around any type.
+	virtualName: string;
+	virtualType: Type;
 }
 
 //TODO potentially think of better name than schema handler.
-export default class SchemaHandler {
+//TODO will need a public getSchema function so user can actually return it.
+export class SchemaHandler {
 
-  readonly schema: Schema;
+	readonly schema: Schema;
 
-  constructor(definition: SchemaDefinition, schemaOptions?: SchemaOptions) {
-    this.schema = this.handleSchemaInitialisation(definition, schemaOptions);
-  }
-
-  public attachVirtuals(virtuals: Virtual[]) {
-
-	forEach(virtuals, virtual => {
-
-	  this.schema.virtual(virtual.virtualName).get(virtual.virtualFunction);
-
-	});
-
-  }
-
-  //TODO think about whether a way for easily adding virtuals, methods etc to the schema is viable.
-  //TODO this will probably mean changing this to a class.
-  private handleSchemaInitialisation(definition: SchemaDefinition, schemaOptions?: SchemaOptions): Schema {
-
-	this.handleErrorCases(definition);
-
-	return new Schema(definition, schemaOptions);
-
-  }
-
-  private handleErrorCases(definition: SchemaDefinition) {
-
-	if (!definition) {
-	  throw new Error('Invalid argument for definition parameter');
+	constructor(definition: SchemaDefinition, schemaOptions?: SchemaOptions) {
+		this.schema = this.handleSchemaInitialisation(definition, schemaOptions);
 	}
 
-	if (Array.isArray(definition) || typeof definition !== 'object') {
-	  throw new Error('Invalid schema type, must be an object');
+	public attachVirtuals(virtuals: Virtual[]) {
+
+		forEach(virtuals, virtual => {
+
+			this.schema.virtual(virtual.virtualName)[virtual.virtualType](virtual.virtualFunction);
+
+		});
+
 	}
 
-	if (Object.keys(definition).length === 0) {
-	  throw new Error('Invalid argument, definition object must contain contents');
+	private handleSchemaInitialisation(definition: SchemaDefinition, schemaOptions?: SchemaOptions): Schema {
+
+		this.handleErrorCases(definition);
+
+		return new Schema(definition, schemaOptions);
+
 	}
 
-  }
+	private handleErrorCases(definition: SchemaDefinition) {
+
+		if (!definition) {
+			throw new Error('Invalid argument for definition parameter');
+		}
+
+		if (Array.isArray(definition) || typeof definition !== 'object') {
+			throw new Error('Invalid schema type, must be an object');
+		}
+
+		if (Object.keys(definition).length === 0) {
+			throw new Error('Invalid argument, definition object must contain contents');
+		}
+
+	}
 
 }
 
