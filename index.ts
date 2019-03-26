e3c4e3251c8c04a4fba22e58234819a7be37faa2
reecemillsom <reece@heydoc.co.uk>
2019-03-26 18:13:38 +0000

    Creating export functions and classes for the user to make use of in index.ts. Updating package.json to reflect a proper named module, changed MongooseClient to mongoose-client.

---
+++ index.ts
@@ -0,0 +1,26 @@
+/*Util functions*/
+import {connectToDB} from "./src/Utils/ConnectToDb";
+import {disconnectFromDb} from "./src/Utils/DisconnectFromDb";
+import {dropCollection, dropMultipleCollections} from "./src/Utils/DropCollection";
+import {dropDatabase} from "./src/Utils/DropDatabase";
+
+/*Schema and Model*/
+import {SchemaHandler} from "./src/Schema/Schema";
+import {Type, VirtualMethod, InstanceMethod, StaticMethod, QueryMethod} from "./src/Schema/Schema";
+import {ModelHandler} from "./src/Model/Model";
+
+
+export {
+	connectToDB,
+	disconnectFromDb,
+	dropCollection,
+	dropMultipleCollections,
+	dropDatabase,
+	SchemaHandler,
+	Type,
+	VirtualMethod,
+	InstanceMethod,
+	StaticMethod,
+	QueryMethod,
+	ModelHandler
+}
